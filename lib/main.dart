4721d487d727c01d030e02860a3e88ff8ff49a62
Tymoteusz Buczkowski <tymoteusz.buczkowski@droidsonroids.pl>
2023-02-03 21:36:08 +0100

    Add knight

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,82 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       home: BonfireWidget(
+        player: Knight(
+          Vector2((4 * DungeonMap.tileSize), (6 * DungeonMap.tileSize)),
+        ),
+        joystick: Joystick(
+          keyboardConfig: KeyboardConfig(),
+        ),
         map: DungeonMap.map(),
+      ),
+    );
+  }
+}
+
+class PlayerSpriteSheet {
+  static Future<SpriteAnimation> get idleLeft => SpriteAnimation.load(
+        "player/knight_idle_left.png",
+        SpriteAnimationData.sequenced(
+          amount: 6,
+          stepTime: 0.1,
+          textureSize: Vector2(16, 16),
+        ),
+      );
+
+  static Future<SpriteAnimation> get idleRight => SpriteAnimation.load(
+        "player/knight_idle.png",
+        SpriteAnimationData.sequenced(
+          amount: 6,
+          stepTime: 0.1,
+          textureSize: Vector2(16, 16),
+        ),
+      );
+
+  static Future<SpriteAnimation> get runRight => SpriteAnimation.load(
+        "player/knight_run.png",
+        SpriteAnimationData.sequenced(
+          amount: 6,
+          stepTime: 0.1,
+          textureSize: Vector2(16, 16),
+        ),
+      );
+
+  static Future<SpriteAnimation> get runLeft => SpriteAnimation.load(
+        "player/knight_run_left.png",
+        SpriteAnimationData.sequenced(
+          amount: 6,
+          stepTime: 0.1,
+          textureSize: Vector2(16, 16),
+        ),
+      );
+
+  static SimpleDirectionAnimation get simpleDirectionAnimation => SimpleDirectionAnimation(
+        idleRight: idleRight,
+        runRight: runRight,
+      );
+}
+
+class Knight extends SimplePlayer with ObjectCollision {
+  Knight(Vector2 position)
+      : super(
+          position: position,
+          size: Vector2(32, 32),
+          animation: PlayerSpriteSheet.simpleDirectionAnimation,
+        ) {
+    setupCollision(
+      CollisionConfig(
+        collisions: [
+          CollisionArea.rectangle(
+            size: Vector2(
+              DungeonMap.tileSize / 2,
+              DungeonMap.tileSize / 2.2,
+            ),
+            align: Vector2(
+              DungeonMap.tileSize / 3.5,
+              DungeonMap.tileSize / 2,
+            ),
+          )
+        ],
       ),
     );
   }
