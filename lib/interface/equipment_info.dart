c9ba0531dc3f51589e8f2f45a7483b623642aa80
Michal Wadolowski <michal.wadolowski@newcommerce.com.pl>
2023-02-04 19:35:20 +0100

    iteki w inentory i na planszy pokazjo lvl requirement

--- lib/interface/equipment_info.dart
+++ lib/interface/equipment_info.dart
@@ -1,12 +1,15 @@
 import 'dart:ui';
 
 import 'package:bonfire/bonfire.dart';
+import 'package:collection/collection.dart';
+import 'package:flutter/material.dart';
+import 'package:hackerspace_game_jam_2023/equipment/base_equipment.dart';
 
 import '../equipment/base_item.dart';
 import '../overworld/player.dart';
 
 class EquipmentInfo extends InterfaceComponent {
-  List<InteractableItem> currentEquipment = [];
+  List<BaseWeapon> currentEquipment = [];
 
   EquipmentInfo() : super(
     id: 2,
@@ -23,9 +26,16 @@ class EquipmentInfo extends InterfaceComponent {
 
   @override
   void render(Canvas c) {
-    currentEquipment.asMap().forEach((index, value) {
-      double offsetX = 80 + index * 30;
-      value.sprite?.render(c, size: Vector2(20, 20), position: Vector2(offsetX, 40));
-    });
+    var style = const TextStyle(color: Colors.white, fontSize: 10);
+    currentEquipment
+        .whereNot((element) => element is DummyWeapon)
+        .toList()
+        .asMap()
+        .forEach((index, value) {
+          double offsetX = 80 + index * 30;
+          value.sprite?.render(c, size: Vector2(20, 20), position: Vector2(offsetX, 40));
+          var itemLvl = value.reqLvl;
+          TextPaint(style: style).render(c, "LV: $itemLvl", Vector2(offsetX, 65));
+        });
   }
 }
